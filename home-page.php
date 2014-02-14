455795382863d6d4b7ce86bd2d6a7d3698be74a1
James Sacci <james.sacci@prowebinnovations.com>
2014-02-14 16:29:59 -0500

    added custom partials/loop-home.php

--- home-page.php
+++ home-page.php
@@ -14,7 +14,7 @@ Template Name: Home Page
 					
 					    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
-					    	<?php get_template_part( 'partials/loop', 'page' ); ?>
+					    	<?php get_template_part( 'partials/loop', 'home' ); ?>
 					    					
 					    <?php endwhile; else : ?>
 					
---
+++ partials/loop-home.php
@@ -0,0 +1,8 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+											
+    <section class="entry-content clearfix" itemprop="articleBody">
+	    <?php the_content(); ?>
+	    <?php wp_link_pages(); ?>
+	</section> <!-- end article section -->
+					
+</article> <!-- end article -->
