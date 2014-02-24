5884e9debaad5b47ae0c6a505dbe96a020cafe01
James Sacci <james.sacci@prowebinnovations.com>
2014-02-24 15:15:23 -0500

    added equalizer to Home Widgets

--- home-page.php
+++ home-page.php
@@ -29,14 +29,14 @@ Template Name: Home Page
                             </div>
                             </div>
                             
-                            <div class="row home-blocks-container">
-                            <div class="large-4 columns home-widget-container">
+                            <div class="row home-blocks-container" data-equalizer>
+                            <div class="large-4 columns home-widget-container" data-equalizer-watch>
                             <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Homeleft')) : endif; ?>
                             </div>
-                            <div class="large-4 columns home-widget-container">
+                            <div class="large-4 columns home-widget-container" data-equalizer-watch>
                             <div class="flex-video"><?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Homecenter')) : endif; ?></div>
                             </div>
-                            <div class="large-4 columns home-widget-container">
+                            <div class="large-4 columns home-widget-container" data-equalizer-watch>
                             
                             <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Homeright')) : endif; ?>
                             
